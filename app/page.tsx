3fb17a1f0579e873f0ab749969c8d3ab8a978394
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-12-03 14:01:47 -0400

    Add background image and apply styling to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+import Image from 'next/image';
+import bg from './images/background.jpeg';
 
-export default function Home() {
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  );
+export default function Page() {
+  return(
+    <div>
+      <Image
+        src={bg}
+        fill 
+        alt='background'
+        sizes='100vw'
+        style={{
+          objectFit: 'cover',
+          zIndex: -1
+        }}
+      
+      />
+
+    </div>
+  )
 }
+
