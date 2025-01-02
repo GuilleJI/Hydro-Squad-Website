08457a7b7d02a84d7f5b98e7ceca4ad4d623692b
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2025-01-02 16:58:12 -0400

    Refactor Services page layout; enhance text styling and accessibility features

--- app/Routing/gallery/page.tsx
+++ app/Routing/gallery/page.tsx
@@ -15,106 +15,106 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function Gallery() {
     return (
         <>
-            
-            <div className="container-fluid p-5 bg-primary text white text-center" style={{ backgroundImage: `url(${bg.src})`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat", }}>
-                <h1 className="text-white d-flex p-2">Our Photo Gallery</h1>
-                <p  className="text-white d-flex p-2">Before and After</p>
-            </div>
-            <br/><br/>
-            <div>
-                <p className="container mt-5" style={{ maxWidth: "800px"}}>
-                    <strong>They say a picture speaks volumes</strong>. At Hydro Squad, we believe in complete transparency, which is why we take detailed digital photos throughout every restaurant hood and kitchen exhaust cleaning. Once the job is finished, we share a link so our customers can easily view the results. Want to see the difference for yourself? Use the slider below to compare our 'before' and 'after' photos!
-                </p>
-            </div>
-            <br/><br/>
+            <div className="bg-dark text-white">
+                <div className="container-fluid p-5 text white text-center" style={{ backgroundImage: `url(${bg.src})`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat", zIndex: -1 }}>
+                    <h1 className="text-white d-flex p-2">Our Photo Gallery</h1>
+                    <p  className="text-white d-flex p-2">Before and After</p>
+                </div>
+                <br/><br/>
+                <div>
+                    <p className=" lead container mt-5" style={{ maxWidth: "800px"}}>
+                        <strong>They say a picture speaks volumes</strong>. At Hydro Squad, we believe in complete transparency, which is why we take detailed digital photos throughout every restaurant hood and kitchen exhaust cleaning. Once the job is finished, we share a link so our customers can easily view the results. Want to see the difference for yourself? Use the slider below to compare our 'before' and 'after' photos!
+                    </p>
+                </div>
+                <br/><br/>
 
-            {/* Carousel for the gallery */}
-            <div id="gallery" className="carousel slide carousel-fade mx-auto" data-bs-ride="carousel" style={{ maxWidth: "800px", width: "100%"}}>
-                {/* Indicators */}
-                <div className="carousel-indicators bg-primary.bg-gradient bg-dark">
-                    <button type="button" data-bs-target="#gallery" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#gallery" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#gallery" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                    <button type="button" data-bs-target="#gallery" data-bs-slide-to="3" aria-label="Slide 4"></button>
-                    <button type="button" data-bs-target="#gallery" data-bs-slide-to="4" aria-label="Slide 5"></button>
-                    <button type="button" data-bs-target="#gallery" data-bs-slide-to="5" aria-label="Slide 6"></button>
+                {/* Carousel for the gallery */}
+                <div id="gallery" className="carousel slide carousel-fade mx-auto" data-bs-ride="carousel" style={{ maxWidth: "800px", width: "100%"}}>
+                    {/* Indicators */}
+                    <div className="carousel-indicators bg-primary.bg-gradient bg-dark">
+                        <button type="button" data-bs-target="#gallery" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#gallery" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#gallery" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        <button type="button" data-bs-target="#gallery" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                        <button type="button" data-bs-target="#gallery" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                        <button type="button" data-bs-target="#gallery" data-bs-slide-to="5" aria-label="Slide 6"></button>
+                    </div>
+                    {/* Carousel inner */}
+                    <div className="carousel-inner text-center p-6 rounded-4" role="listbox">
+                        {/* Before and After images */}
+                        <div className="carousel-item active">
+                            <Image
+                                className="d-block mx-auto img-fluid"
+                                alt="Before"
+                                src={b4}
+                            />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h3 className="bg-black  bg-opacity-50">Before</h3>
+                            </div>
+                        </div>
+                        <div className="carousel-item ">
+                            <Image
+                                className="d-block mx-auto img-fluid"
+                                alt="After"
+                                src={aft1}
+                            />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h3 className="bg-black  bg-opacity-50">After</h3>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <Image
+                                className="d-block mx-auto img-fluid"
+                                alt="Before"
+                                src={b4_2}
+                            />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h3 className="bg-black  bg-opacity-50">Before</h3>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <Image
+                                className="d-block mx-auto img-fluid"
+                                alt="After"
+                                src={aft2}
+                            />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h3 className="bg-black  bg-opacity-50">After</h3>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <Image
+                                className="d-block mx-auto img-fluid"
+                                alt="Before"
+                                src={b4_3}
+                            />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h3 className="bg-black  bg-opacity-50">Before</h3>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <Image
+                                className="d-block mx-auto img-fluid"
+                                alt="After"
+                                src={aft3}
+                            />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h3 className="bg-black  bg-opacity-50">After</h3>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Carousel controls */}
+                    <a className="carousel-control-prev p-3 bg-light rounded-start-4 " href="#gallery" role="button" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon bg-dark rounded-circle" aria-hidden="true"></span>
+                        <span className="sr-only"></span>
+                    </a>
+                    <a className="carousel-control-next p-3 bg-light rounded-end-4" href="#gallery" role="button" data-bs-slide="next">
+                        <span className="carousel-control-next-icon bg-dark rounded-circle" aria-hidden="true"></span>
+                        <span className="sr-only"></span>
+                    </a>
                 </div>
-                {/* Carousel inner */}
-                <div className="carousel-inner text-center p-6" role="listbox">
-                    {/* Before and After images */}
-                    <div className="carousel-item active">
-                        <Image
-                            className="d-block mx-auto img-fluid"
-                            alt="Before"
-                            src={b4}
-                        />
-                        <div className="carousel-caption d-none d-md-block">
-                            <h3 className="bg-black  bg-opacity-50">Before</h3>
-                        </div>
-                    </div>
-                    <div className="carousel-item ">
-                        <Image
-                            className="d-block mx-auto img-fluid"
-                            alt="After"
-                            src={aft1}
-                        />
-                        <div className="carousel-caption d-none d-md-block">
-                            <h3 className="bg-black  bg-opacity-50">After</h3>
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <Image
-                            className="d-block mx-auto img-fluid"
-                            alt="Before"
-                            src={b4_2}
-                        />
-                        <div className="carousel-caption d-none d-md-block">
-                            <h3 className="bg-black  bg-opacity-50">Before</h3>
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <Image
-                            className="d-block mx-auto img-fluid"
-                            alt="After"
-                            src={aft2}
-                        />
-                        <div className="carousel-caption d-none d-md-block">
-                            <h3 className="bg-black  bg-opacity-50">After</h3>
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <Image
-                            className="d-block mx-auto img-fluid"
-                            alt="Before"
-                            src={b4_3}
-                        />
-                        <div className="carousel-caption d-none d-md-block">
-                            <h3 className="bg-black  bg-opacity-50">Before</h3>
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <Image
-                            className="d-block mx-auto img-fluid"
-                            alt="After"
-                            src={aft3}
-                        />
-                        <div className="carousel-caption d-none d-md-block">
-                            <h3 className="bg-black  bg-opacity-50">After</h3>
-                        </div>
-                    </div>
-                </div>
-                {/* Carousel controls */}
-                <a className="carousel-control-prev p-3 bg-dark" href="#gallery" role="button" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="sr-only"></span>
-                </a>
-                <a className="carousel-control-next p-3 bg-dark" href="#gallery" role="button" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="sr-only"></span>
-                </a>
-            </div>
-            <br/><br/>
-        
+                <br/><br/>
+            </div>    
         </>
     )
 }; 
--- app/Routing/services/page.tsx
+++ app/Routing/services/page.tsx
@@ -9,51 +9,56 @@ import bg from '../../images/man-doing-professional-home-cleaning-service.jpg';
 export default function Services() {
     return (
         <>
-            <div className="container-fluid p-5 bg-primary text white text-center" style={{ backgroundImage: `url(${bg.src})`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat", }}>
-                <h1 className="text-white d-flex p-4">Our Services</h1>
-                {/* <p  className="text-white d-flex p-2"></p> */}
-            </div>
+            <div className="bg-dark text-white">
+                <div className="container-fluid p-5 bg-primary text white text-center" style={{ backgroundImage: `url(${bg.src})`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat", }}>
+                    <h1 className="text-white d-flex p-4">Our Services</h1>
+                    {/* <p  className="text-white d-flex p-2"></p> */}
+                </div>
 
-            <div className="container">
-                <div className="row">
-                    <div className="col w-50 p-5 m-3">
-                        <h2><strong>Professional Cleaning Services for Your Business</strong></h2>
-                        <br/>
-                        <p>Experience our top-tier commercial cleaning services, including kitchen hood and exhaust cleaning, as well as power washing, across Ontario (including GTA). We ensure your business adheres to all <strong>NFPA 94</strong> and fire Marshall regulations, providing you with peace of mind and a spotless environment. </p>
-                        <br/><br/>
-                        <div className="text-center">
-                            <Image
-                            alt="NFPA 94" 
-                            src={logo}
-                            width="300" 
-                            height="350"
-                            />
-                        </div>
-                        
-                        <br/>
+                <div className="container">
+                    <div className="row">
+                        <div className="col w-50 p-5 m-3">
+                            <h2><strong>Professional Cleaning Services for Your Business</strong></h2>
+                            <br/>
+                            <p className="lead">Experience our top-tier commercial cleaning services, including kitchen hood and exhaust cleaning, as well as power washing, across Ontario (including GTA). We ensure your business adheres to all <strong>NFPA 94</strong> and fire Marshall regulations, providing you with peace of mind and a spotless environment. </p>
+                            <br/><br/>
+                            <div className="text-center bg-light rounded-4 p-2">
+                                <Image
+                                className="img-fluid"
+                                alt="NFPA 94" 
+                                src={logo}
+                                width="300" 
+                                height="350"
+                                />
+                            </div>
+                            
+                            <br/>
 
-                    </div>
-                    <div className="col w-50 p-5 m-3">
-                        <div className="text-center">
-                            <Image 
-                            alt="Hood Cleaning" 
-                            src={hood}
-                            width="420" 
-                            height="300"
-                            />
                         </div>
-                        
-                        <br/> 
-                        <br/>
-                        <ul className="list-inline">
-                            <li className="list-inline-item"><strong className="text-danger">Commercial Kitchen Cleaning</strong> - Ensures a hygienic and safe environment by thoroughly cleaning all kitchen surfaces, equipment, and areas to meet health and safety standards. </li>
-                            <br/><br/>
-                            <li className="list-inline-item"><strong className="text-danger">Filter cleaning and maintenance</strong> - Regularly cleans and maintains filers to ensure optimal performance and air quality, preventing the buildup of grease and other contaminants that can pose fire hazards.</li>
-                            <br/><br/>
-                            <li className="list-inline-item"><strong className="text-danger">Hood, Vent, Exhaust Cleaning</strong> - Involves the thorough cleaning of kitchen hoods, vents, and exhaust systems to remove grease and other residues, ensuring proper ventilation and reducing the risk of fire.</li>
-                            <br/><br/>
-                            <li className="list-inline-item"><strong className="text-danger">Grease traps cleaning</strong> - Regularly cleans and maintains filters to ensure optimal performance and air quality, preventing the buildup of grease and other contaminants that can pose fire hazards.</li>
-                        </ul>
+                        <div className="col w-50 p-5 m-3">
+                            <div className="text-center">
+                                <Image 
+                                className="rounded-4 img-fluid"
+                                alt="Hood Cleaning" 
+                                src={hood}
+                                width="470" 
+                                height="300"
+                                />
+                            </div>
+                            <br/> 
+                            <br/>
+                            <h2><strong>Transform Your Kitchen with Our Professional Cleaning Services!</strong></h2>
+                            <br/>
+                            <ul className="list-inline">
+                                <li className=" lead list-inline-item"><strong className="text-danger">✨ Commercial Kitchen Cleaning</strong> - We Keep your kitchen spotless, safe, and up to code with our deep-cleaning services for all surfaces, equipment, and workspaces. </li>
+                                <br/><br/>
+                                <li className="lead list-inline-item"><strong className="text-danger">🌬️ Filter Cleaning & Maintenance</strong> -Breathe easier and stay fire-safe! We clean and maintain filters to improve air quality and prevent grease buildup.</li>
+                                <br/><br/>
+                                <li className="lead list-inline-item"><strong className="text-danger">🔥 Hood, Vent & Exhaust Cleaning</strong> - Eliminate grease and fire hazards with our thorough cleaning of hoods, vents, and exhaust systems—ensuring proper ventilation and safety.</li>
+                                <br/><br/>
+                                <li className="lead list-inline-item"><strong className="text-danger">💧 Grease Trap Cleaning</strong> - Avoid clogs, odors, and compliance issues with expert grease trap cleaning and maintenance that keeps your kitchen running smoothly.</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
