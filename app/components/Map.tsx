2412c136a56bf49b014b8e7ac80aba6f8bb0b804
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2025-01-16 20:18:05 -0400

    Initialize map component with useEffect for asynchronous loading

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -5,6 +5,18 @@ import React, { useEffect } from 'react'; // Import the useEffect hook from Reac
 
 export function Map() {
 
+    const mapRef = React.useRef(null); // Create a reference to the map
+
+    // useEffect hook to run code after the component has rendered
+    useEffect(() => {
+
+        // Function to initialize the map..async is used to make the function asynchronous because it will take some time to load the map and we don't want to block the rendering of the component
+        const initMap = async () => {
+            console.log('map init'); // Log a message to the console for testing purposes
+        }
+        initMap(); // Call the function to initialize the map
+    },[]);
+
     return (
         <h1>Map</h1>
     )
