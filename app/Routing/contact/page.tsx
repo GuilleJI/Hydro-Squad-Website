f5800389553882d9ca73872ec91656fa7934d1a9
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2025-01-17 23:30:25 -0400

    Update Submit button styling on Contact page for improved visibility and user interaction

--- app/Routing/contact/page.tsx
+++ app/Routing/contact/page.tsx
@@ -72,7 +72,7 @@ export default function Contact() {
                                 </div>
 
                             {/* Submit button */}
-                                <button type="button" className="btn btn-danger btn-block mb-4 border-2 border-dark">Submit</button>
+                                <button type="button" className="btn btn-primary btn-lg w-100 border-2 border-dark">Submit</button>
                             </form>
                         </div>
                         <div className="col-12 col-md-6">
