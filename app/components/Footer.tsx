58b86dcce8b086ccf9959782d37792d546c95825
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2025-01-22 01:51:37 -0400

    Update Footer component to include navigation links and improve accessibility

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,63 +1,63 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Footer() {
     return (
         <>
-
+            {/* // Add a footer for the application */}
             <footer className="text-center text-white" style={{backgroundColor: "rgb(14, 46, 100)"}}>
-
+                {/* // Add a container for the footer */}
                 <div className="container">
-
+                    {/* // Add a section for the footer */}
                     <section>
-
+                        {/* // Add a row for the footer */}
                         <div className="row text-center d-flex justify-content-center pt-5">
 
-                        <div className="col-md-2">
-                            <h6 className="text-uppercase font-weight-bold">
-                            <a href="#!" className="text-white">About us</a>
-                            </h6>
+                            {/* // Add a link to the home page */}
+                            <div className="col-md-2">
+                                <h6 className="text-uppercase font-weight-bold">
+                                <Link href="/" className="text-white">Home</Link>
+                                </h6>
+                            </div>
+
+
+                            {/* // Add a link to the services page */}
+                            <div className="col-md-2">
+                                <h6 className="text-uppercase font-weight-bold">
+                                <Link href="/Routing/services" className="text-white">Services</Link>
+                                </h6>
+                            </div>
+
+
+                            {/* // Add a link to the gallery page */}
+                            <div className="col-md-2">
+                                <h6 className="text-uppercase font-weight-bold">
+                                <Link href="/Routing/gallery" className="text-white">Gallery</Link>
+                                </h6>
+                            </div>
+
+
+                            {/* // Add a link to the about page */}
+                            <div className="col-md-2">
+                                <h6 className="text-uppercase font-weight-bold">
+                                <Link href="/Routing/about" className="text-white">About Us</Link>
+                                </h6>
+                            </div>
+
+
+                            {/* // Add a link to the contact page */}
+                            <div className="col-md-2">
+                                <h6 className="text-uppercase font-weight-bold">
+                                <Link href="/Routing/contact" className="text-white">Contact</Link>
+                                </h6>
+                            </div>
                         </div>
-
-
-
-                        <div className="col-md-2">
-                            <h6 className="text-uppercase font-weight-bold">
-                            <a href="#!" className="text-white">Products</a>
-                            </h6>
-                        </div>
-
-
-
-                        <div className="col-md-2">
-                            <h6 className="text-uppercase font-weight-bold">
-                            <a href="#!" className="text-white">Awards</a>
-                            </h6>
-                        </div>
-
-
-
-                        <div className="col-md-2">
-                            <h6 className="text-uppercase font-weight-bold">
-                            <a href="#!" className="text-white">Help</a>
-                            </h6>
-                        </div>
-
-
-
-                        <div className="col-md-2">
-                            <h6 className="text-uppercase font-weight-bold">
-                            <a href="#!" className="text-white">Contact</a>
-                            </h6>
-                        </div>
-
-                        </div>
-
                     </section>
 
-
+                    {/* Add a horizontal rule */}
                     <hr className="my-5" />
 
-                
+                    {/* Add a section for the footer */}
                     <section className="mb-5">
                         <div className="row d-flex justify-content-center">
                         <div className="col-lg-8">
@@ -71,7 +71,7 @@ export default function Footer() {
                         </div>
                     </section>
 
-                    <section className="text-center mb-5">
+                    {/* <section className="text-center mb-5">
                         <a href="" className="text-white me-4">
                         <i className="fab fa-facebook-f"></i>
                         </a>
@@ -90,7 +90,7 @@ export default function Footer() {
                         <a href="" className="text-white me-4">
                         <i className="fab fa-github"></i>
                         </a>
-                    </section>
+                    </section> */}
 
                 </div>
 
@@ -98,7 +98,7 @@ export default function Footer() {
                         className="text-center p-3"
                         style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}
                         >
-                    © 2025 Copyright:
+                        © 2025 Copyright: Hydro Squad Kitchen Exhaust Cleaning
                     </div>
                 </footer>   
  
